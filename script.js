8ae501b7a0da2ba319fa27b0094957e08d460d98
Alexander Kurz <alexhkurz@gmail.com>
2023-10-10 12:31:10 -0700

    aider: Added function to find the position of the empty tile and modified movePiece function to swap the clicked piece with the empty tile if they are adjacent.
    # Aider chat conversation:
    
    ASSISTANT: To implement this functionality, we need to modify the `movePiece` function in the `script.js` file. We will also need to add a new function to find the position of the empty tile.
    
    Here are the changes:
    
    ```javascript
    script.js
    <<<<<<< HEAD
    function movePiece(piece) {
        // Code to move a piece will go here
    }
    =======
    function getEmptyTilePosition() {
        for (var i = 0; i < 4; i++) {
            for (var j = 0; j < 4; j++) {
                if (puzzle[i][j] === 0) {
                    return { i: i, j: j };
                }
            }
        }
    }
    
    function movePiece(piece) {
        var pieceNumber = parseInt(piece.textContent);
        var piecePosition;
        var emptyTilePosition = getEmptyTilePosition();
    
        for (var i = 0; i < 4; i++) {
            for (var j = 0; j < 4; j++) {
                if (puzzle[i][j] === pieceNumber) {
                    piecePosition = { i: i, j: j };
                    break;
                }
            }
        }
    
        if (Math.abs(piecePosition.i - emptyTilePosition.i) + Math.abs(piecePosition.j - emptyTilePosition.j) === 1) {
            puzzle[piecePosition.i][piecePosition.j] = 0;
            puzzle[emptyTilePosition.i][emptyTilePosition.j] = pieceNumber;
            piece.style.left = (emptyTilePosition.j * 100) + 'px';
            piece.style.top = (emptyTilePosition.i * 100) + 'px';
        }
    }
    >>>>>>> updated
    ```
    
    This change will add a `getEmptyTilePosition` function to find the position of the empty tile in the puzzle. The `movePiece` function will then swap the clicked piece with the empty tile if they are adjacent.

--- script.js
+++ script.js
@@ -9,8 +9,36 @@ function shufflePuzzle() {
     // Code to shuffle the puzzle will go here
 }
 
+function getEmptyTilePosition() {
+    for (var i = 0; i < 4; i++) {
+        for (var j = 0; j < 4; j++) {
+            if (puzzle[i][j] === 0) {
+                return { i: i, j: j };
+            }
+        }
+    }
+}
+
 function movePiece(piece) {
-    // Code to move a piece will go here
+    var pieceNumber = parseInt(piece.textContent);
+    var piecePosition;
+    var emptyTilePosition = getEmptyTilePosition();
+
+    for (var i = 0; i < 4; i++) {
+        for (var j = 0; j < 4; j++) {
+            if (puzzle[i][j] === pieceNumber) {
+                piecePosition = { i: i, j: j };
+                break;
+            }
+        }
+    }
+
+    if (Math.abs(piecePosition.i - emptyTilePosition.i) + Math.abs(piecePosition.j - emptyTilePosition.j) === 1) {
+        puzzle[piecePosition.i][piecePosition.j] = 0;
+        puzzle[emptyTilePosition.i][emptyTilePosition.j] = pieceNumber;
+        piece.style.left = (emptyTilePosition.j * 100) + 'px';
+        piece.style.top = (emptyTilePosition.i * 100) + 'px';
+    }
 }
 
 document.addEventListener('DOMContentLoaded', function() {
