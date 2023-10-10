e50238826c719a34e5c5d3ec6b85e8c68904c931
Alexander Kurz <alexhkurz@gmail.com>
2023-10-10 12:26:31 -0700

    aider: Added logic for creating the puzzle pieces, positioning them correctly, and adding click event listeners to each piece.
    # Aider chat conversation:
    
    USER: what is next?
    
    ASSISTANT: Next, we will add the logic for the sliding puzzle in the `script.js` file. We will create a 3x3 grid and fill it with numbers from 1 to 8, leaving one cell empty. The empty cell will be represented by the number 0. We will also add the logic to shuffle the puzzle and to move a piece when it is clicked.
    
    Here are the changes for the `script.js` file:
    
    ```javascript
    script.js
    <<<<<<< HEAD
    document.addEventListener('DOMContentLoaded', function() {
        // Code to initialize the puzzle will go here
    });
    
    document.getElementById('shuffle-button').addEventListener('click', function() {
        // Code to shuffle the puzzle will go here
    });
    =======
    var puzzle = [
        [1, 2, 3],
        [4, 5, 6],
        [7, 8, 0]
    ];
    
    function shufflePuzzle() {
        // Code to shuffle the puzzle will go here
    }
    
    function movePiece(piece) {
        // Code to move a piece will go here
    }
    
    document.addEventListener('DOMContentLoaded', function() {
        var container = document.getElementById('puzzle-container');
        for (var i = 0; i < 3; i++) {
            for (var j = 0; j < 3; j++) {
                var piece = document.createElement('div');
                piece.classList.add('puzzle-piece');
                piece.textContent = puzzle[i][j];
                piece.style.left = (j * 100) + 'px';
                piece.style.top = (i * 100) + 'px';
                piece.addEventListener('click', function() {
                    movePiece(this);
                });
                container.appendChild(piece);
            }
        }
    });
    
    document.getElementById('shuffle-button').addEventListener('click', shufflePuzzle);
    >>>>>>> updated
    ```
    
    This code creates the puzzle pieces and positions them in the correct place. It also adds a click event listener to each piece, which will call the `movePiece` function when the piece is clicked. The `shufflePuzzle` function is called when the "Shuffle" button is clicked, but it doesn't do anything yet. We will add the logic for these functions next.

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
+var puzzle = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 0]
+];
+
+function shufflePuzzle() {
+    // Code to shuffle the puzzle will go here
+}
+
+function movePiece(piece) {
+    // Code to move a piece will go here
+}
+
 document.addEventListener('DOMContentLoaded', function() {
-    // Code to initialize the puzzle will go here
+    var container = document.getElementById('puzzle-container');
+    for (var i = 0; i < 3; i++) {
+        for (var j = 0; j < 3; j++) {
+            var piece = document.createElement('div');
+            piece.classList.add('puzzle-piece');
+            piece.textContent = puzzle[i][j];
+            piece.style.left = (j * 100) + 'px';
+            piece.style.top = (i * 100) + 'px';
+            piece.addEventListener('click', function() {
+                movePiece(this);
+            });
+            container.appendChild(piece);
+        }
+    }
 });
 
-document.getElementById('shuffle-button').addEventListener('click', function() {
-    // Code to shuffle the puzzle will go here
-});
+document.getElementById('shuffle-button').addEventListener('click', shufflePuzzle);
